5167471e3d4c46faca2b6208979e54ad533a1c32
ayeshayusufgit <ayesha.yusuf@gmail.com>
2023-10-05 14:00:31 +0530

    Adding the review comments

--- src/main/java/com/democart/qa/pages/ProfilePage.java
+++ src/main/java/com/democart/qa/pages/ProfilePage.java
@@ -1,5 +1,13 @@
 package com.democart.qa.pages;
 
+import org.openqa.selenium.By;
+
 public class ProfilePage {
+	
+	By profileIcon = By.name("hello");
+	
+	public void createProfile() {
+		System.out.println("Creating a profile");
+	}
 
 }
--- src/test/java/com/democart/qa/tests/TestDemo.java
+++ src/test/java/com/democart/qa/tests/TestDemo.java
@@ -2,6 +2,7 @@ package com.democart.qa.tests;
 
 public class TestDemo {
 
+	//Adding the comments as per the review
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test demo....");
